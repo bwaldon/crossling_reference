764c4704cc13a87f0fd54139165cb5fccccaef3e
bwaldon <bwaldon@stanford.edu>
2021-05-05 21:12:09 -0500

    change instructions logic

--- experiments/from-scratch/client/intro/InstructionStepThree.jsx
+++ experiments/from-scratch/client/intro/InstructionStepThree.jsx
@@ -8,18 +8,22 @@ export default class InstructionStepTwo extends React.Component {
     return (
       <Centered>
         <div className="instructions">
-          <h1> Game instructions (part 2 of 2) </h1>
+          <h1> Game instructions (part 3 of 3) </h1>
 
-          <center>
-          <img src = "directorsample.png" width = "600px" />
-          </center>
-
-          <p> 
-          Once the speaker sends a message and the listener clicks on the object they believe is the target, both partners are briefly shown which object the guesser clicked on. If the guesser selects an incorrect image, that incorrect selection will be highlighted in <style = "font:red"> red </style>. 
+           <p> 
+          Once the director sends a message and the guesser selects the object they believe to be the target, both partners are briefly shown which object the guesser clicked on. At this stage, the correct image is highlighted in <font color = "green">green</font>. If the guesser selects an incorrect image, that incorrect selection will be highlighted in <font color = "red"> red</font>. 
           </p>
 
           <p>
-         After the guesser makes a selection, you will both be automatically forwarded to the next round of objects. There are a total of 72 rounds. After the 72nd round, you will fill out an optional brief survey and receive a participation code to enter on Prolific.
+          Here's the guesser's perspective from the round you just saw. On this round, the guesser correctly identified the target:
+          </p>
+
+          <center>
+          <img src = "guessersample.png" width = "650px" />
+          </center>
+
+          <p>
+         After reviewing the guesser's selection, you will both be automatically forwarded to the next round of objects. There are a total of 72 rounds. After the 72nd round, you will fill out an optional brief survey and receive a participation code to enter on Prolific.
          </p>
 
           <p>
