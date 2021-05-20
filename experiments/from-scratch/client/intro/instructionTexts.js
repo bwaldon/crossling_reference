43380344c9fcddb8558baae49c3d956343d8b4e5
ycm <ycm@stanford.edu>
2021-05-20 14:12:59 -0700

    very minor edit to make it more straightforward for qi to paste in chinese translations

--- experiments/from-scratch/client/intro/instructionTexts.js
+++ experiments/from-scratch/client/intro/instructionTexts.js
@@ -55,6 +55,11 @@ export const instructionsStepThreeTexts = {
     },
 
     'ChineseSimplified': {
-
+        'instructionTitle': 'Game instructions (part 3 of 3)',
+        'instructionLine1': "Once the director sends a message and the guesser selects the object they believe to be the target, both partners are briefly shown which object the guesser clicked on. At this stage, the correct image is highlighted in " + "green".fontcolor('green') + "If the guesser selects an incorrect image, that incorrect selection will be highlighted in " + "red".fontcolor('red') + ". ",
+        'instructionLine2': "Here's the guesser's perspective from the round you just saw. On this round, the guesser correctly identified the target:",
+        'instructionLine3': "After reviewing the guesser's selection, you will both be automatically forwarded to the next round of objects. There are a total of 72 rounds. After the 72nd round, you will fill out an optional brief survey and receive a participation code to enter on Prolific.",
+        'previousButtonText': 'Previous',
+        'nextButtonText': 'Next'
     }
 };
