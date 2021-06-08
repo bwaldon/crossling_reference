d823065a864b7146751809c8736663ce23589a08
ycm <ycm@stanford.edu>
2021-06-07 19:25:59 -0700

    added Chinese translation texts only for chatbox items (not implemented in chatbox yet)

---
+++ experiments/from-scratch/client/game/chatTexts.js
@@ -0,0 +1,12 @@
+export const chatTexts = {
+    "English": {
+        "NoMessagesYet": "No messages yet...",
+        "EnterChatMessagePlaceholder": "Enter chat message",
+        "SendButtonText": "Send"
+    },
+    "ChineseSimplified": {
+        "NoMessagesYet": "暂无信息...",
+        "EnterChatMessagePlaceholder": "输入聊天信息",
+        "SendButtonText": "发送"
+    }
+}
