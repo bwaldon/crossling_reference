461510cd0a5eca24ba9cba0e6244d2060fe5f650
leylakursat <leylakursat@gmail.com>
2022-01-31 17:20:02 -0800

    added csv with utterance probabilities

--- analyses/CTSL/analyses/bda_english/BDA.r
+++ analyses/CTSL/analyses/bda_english/BDA.r
@@ -2,6 +2,7 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 library(jsonlite)
 library(rwebppl)
+source("helpers.R")
 
 source("../_shared/BDA_dataprep.R")
 source("../../_shared/inferenceHelpers.R")
@@ -147,7 +148,7 @@ ggsave("results/eng_incrementalContinuousPredictives.png", width = 4, height = 3
 
 save.image("results/eng_results50000burn.RData")
 
-save.image("results/eng_results.RData")
+#save.image("results/eng_results.RData")
 
 # BAYESIAN MODEL COMPARISON: INCREMENTAL VS. GLOBAL 
 
@@ -180,52 +181,3 @@ modelPosterior <- incrementalVGlobalPosteriors %>% filter(Parameter == "incremen
   summarize(posteriorProb = n / sum(n))
 
 View(modelPosterior)
-
-
-######################################################
-# PLOTS FOR THE COGSCI PAPER
-######################################################
-# modification type
-empirical_toplot = d_uncollapsed %>%
-  mutate(size = ifelse(response=="size",1,0)) %>%
-  mutate(color = ifelse(response=="color",1,0)) %>%
-  mutate(size_color = ifelse(response=="size_color",1,0)) %>%
-  select(gameId,roundNumber,condition,response,size,color,size_color) %>%
-  gather(utterance,value,size:size_color) %>%
-  group_by(utterance,condition) %>%
-  summarize(Mean=mean(value)) %>%
-  #summarise(Mean=mean(value),CILow=ci.low(value),CIHigh=ci.high(value)) %>%
-  #ungroup() %>%
-  #mutate(YMin=Mean-CILow,YMax=Mean+CIHigh) %>%
-  mutate(model="empirical")
-
-vanilla_toplot = vanillaPredictives %>%
-  gather(utterance,Mean,size_color:size) %>%
-  mutate(model="vanilla")
-
-continuous_toplot = continuousPredictives %>%
-  gather(utterance,Mean,size_color:size) %>%
-  mutate(model="continuous")
-
-incremental_toplot = incrementalPredictives %>%
-  gather(utterance,Mean,size_color:size) %>%
-  mutate(model="incremental")
-
-incrementalContinuous_toplot = incrementalContinuousPredictives %>%
-  gather(utterance,Mean,size_color:size) %>%
-  mutate(model="incrementalContinuous")
-
-#merge all datasets
-merged = rbind(empirical_toplot,vanilla_toplot,continuous_toplot,incremental_toplot,incrementalContinuous_toplot) %>%
-  mutate(condition=ifelse(condition=="color31","color sufficient",ifelse(condition=="size31","size sufficient",NA))) %>%
-  mutate(language="English")
-
-merged$model = factor(merged$model, levels = c("empirical", "vanilla","continuous","incremental","incrementalContinuous"))
-
-ggplot(merged, aes(x=utterance,y=Mean, fill=model)) +
-  geom_bar(position="dodge", stat = "identity") +
-  facet_grid(~condition)
-
-write.csv(merged, "english_modelComp.csv", row.names=TRUE)
-ggsave("results/modelComparison.png")
-
