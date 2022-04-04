210450f5b2e5f7f286a28efeee383baf647b7831
thegricean <judithdegen@gmail.com>
2022-04-04 11:29:11 -0500

    updated regression analysis

--- analyses/SpanishMain/regression.R
+++ analyses/SpanishMain/regression.R
@@ -13,7 +13,7 @@ source("../_shared/regressionHelpers.r")
 # READ DATA
 # # (dummy data for analysis pipeline creation)
 d = read_delim("../../data/SpanishMain/data_exp1.tsv", delim = "\t")
-d_postraw = read_tsv("../../data/SpanishMain/postManualTypoCorrection.tsv") 
+d_postraw = read_tsv("../../data/SpanishMain/postManualTypoCorrection.tsv") %>% bind_rows(read_tsv("../../data/SpanishMain/postManualTypoCorrection_part2.tsv"))
 
 # get data with linear order annotation
 d_post = d_postraw %>% 
@@ -26,15 +26,32 @@ d = d %>%
     WordOrder %in% c("ANC","NC","ANCS","ANS","NAS","NCAS","NCS","NS","NSC") ~ "post-nominal",
     WordOrder %in% c("ACN","CN","ACSN","ASN","CASN","CSN","SN","SCN","CSN","ASCN") ~ "pre-nominal",
     WordOrder %in% c("CNS","SNC") ~ "split",
-  TRUE ~ "no noun"))
+  TRUE ~ "no noun")) %>% 
+  mutate(interWordOrder = case_when(
+    WordOrder %in% c("ANC","NC","ANCS","ANS","NAS","NCAS","NCS","NS","NSC") ~ "post-nominal",
+    WordOrder %in% c("ACN","CN","ACSN","ASN","CASN","CSN","SN","SCN","CSN","ASCN") ~ "pre-nominal",
+    WordOrder %in% c("CNS","SNC") ~ "split",
+    WordOrder %in% c("ASC","SC") ~ "likely split",
+    WordOrder %in% c("ACS","CS") ~ "likely post-nominal",
+    TRUE ~ "no noun"))
 
+# how many unique linear orders?
+table(d$WordOrder)
 # how many cases of pre-, post-, and split referring exps?
 table(d$redWordOrder)
 # no noun post-nominal 
-# 622          200 
+
+
+# how many cases of pre-, post-, and split referring exps, assuming no-noun cases with linear order CS are post-nominal, and SC are split?
+table(d$interWordOrder)
+# likely post-nominal        likely split             no noun        post-nominal 
+
+
+# only 6 of the 56 dyads used the purely post-nominal strategy with regularity
+table(d$gameid,d$redWordOrder)
 
 # how many cases of conjunction?
-table(d_postraw$comments)
+table(d_postraw$comments) # 19
 
 # PLOT PROPORTION OF REDUNDANT UTTERANCES BY REDUNDANT PROPERTY
 
@@ -83,11 +100,41 @@ pairscor.fnc(centered[,c("redUtterance","SufficientProperty","SceneVariation","L
 
 options(mc.cores = parallel::detectCores())
 
-# # MODEL SPECIFICATION
+# # MODEL SPECIFICATION, reported in xprag abstract
 
 m.b.full = brm(redUtterance ~ cSufficientProperty*cSceneVariation*cLanguage + (1+cSufficientProperty*cSceneVariation|gameid) + (1+cSufficientProperty*cSceneVariation*cLanguage|clickedType), data=centered, family="bernoulli")
 
 summary(m.b.full)
+
+
+# create centered version of English dataset
+centered = cbind(d_english %>% 
+                   select(redUtterance,SufficientProperty,Trial,SceneVariation,gameid,clickedType) %>% 
+                   mutate(SufficientProperty = as.factor(SufficientProperty)),
+                 myCenter(data.frame(d_english %>% 
+                                       select(SufficientProperty, Trial, SceneVariation) %>% 
+                                       mutate(SufficientProperty = as.factor(SufficientProperty)))))
+
+m.b.english = brm(redUtterance ~ cSufficientProperty*cSceneVariation + (1+cSufficientProperty+cSceneVariation|gameid) + (1+cSufficientProperty*cSceneVariation|clickedType), data=centered, family="bernoulli")
+
+summary(m.b.english)
+
+# create centered version of Spanish dataset
+centered = cbind(d %>% 
+                   filter(Language == "Spanish") %>% 
+                   select(redUtterance,SufficientProperty,Trial,SceneVariation,gameid,clickedType) %>% 
+                   mutate(SufficientProperty = as.factor(SufficientProperty)),
+                 myCenter(data.frame(d %>% 
+                                       filter(Language == "Spanish") %>% 
+                                       select(SufficientProperty, Trial, SceneVariation) %>% 
+                                       mutate(SufficientProperty = as.factor(SufficientProperty)))))
+
+
+
+m.b.spanish = brm(redUtterance ~ cSufficientProperty*cSceneVariation + (1+cSufficientProperty*cSceneVariation|gameid) + (1+cSufficientProperty*cSceneVariation|clickedType), data=centered, family="bernoulli")
+
+summary(m.b.spanish)
+
 
 # ONE-SIDED HYPOTHESIS TESTING (EXAMPLE)
 hypothesis(m.b.full, "cLanguage < 0") # hypothesis(m.b.full, "cLanguage < 0"), depending on reference level coding
@@ -96,7 +143,7 @@ hypothesis(m.b.full, "cLanguage < 0") # hypothesis(m.b.full, "cLanguage < 0"), d
 plot(m.b.full, variable = c("cSufficientProperty"))
 
 # AUXILIARY GLMER ANALYSIS
-m.glm = glmer(redUtterance ~ cSufficientProperty*cSceneVariation*cLanguage + (1+cSufficientProperty*cSceneVariation|gameid) + (1+cSufficientProperty*cSceneVariation*cLanguage|clickedType), data=centered, family="bernoulli")
+m.glm = glmer(redUtterance ~ cSufficientProperty*cSceneVariation*cLanguage + (1+cSufficientProperty*cSceneVariation|gameid) + (1+cSufficientProperty*cSceneVariation*cLanguage|clickedType), data=centered, family="binomial")
 summary(m.glm)
 
 
