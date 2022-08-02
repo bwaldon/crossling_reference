f6efe639f52cf7482282f9cc58a9403437a1f843
Angelique <acharles-davis00@stuy.edu>
2022-08-02 10:31:08 -0700

    pulling graphs

---
+++ experiments/VIETNAMESE/2_color_norming/js/index.js
@@ -0,0 +1,258 @@
+//https://www.ncbi.nlm.nih.gov/pmc/articles/doi/10.3389/fpsyg.2014.00399/full// Returns a random integer between min (included) and max (excluded)
+// Using Math.round() will give you a non-uniform distribution!
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function make_slides(f) {
+  var slides = {};
+
+function startsWith(str, substrings) {
+    for (var i = 0; i != substrings.length; i++) {
+       var substring = substrings[i];
+       if (str.indexOf(substring) == 0) {
+         return 1;
+       }
+    }
+    return -1;
+}
+
+function getArticleItem(item_id) {
+  var article = "";
+  if (startsWith(item_id, ["a","e","i","o","u"]) == 1) {
+    article = "an ";
+  } else {
+    article = "a ";
+  }
+  return article;
+}
+
+  slides.i0 = slide({
+     name : "i0",
+     start: function() {
+      exp.startT = Date.now();
+     }
+  });
+
+  slides.objecttrial = slide({
+    name : "objecttrial",
+    present : exp.all_stims,
+    start : function() {
+	   $(".err").hide();
+    },
+      present_handle : function(stim) {
+      console.log(stim);
+    	this.trial_start = Date.now();
+      $(".err").hide();
+      $("#answer").val("");
+      $(".err").hide();
+      // exp.sliderPost = {};
+	   //$("#objectlabel").val("");
+	  this.stim = stim;
+    // stim.item = _.shuffle(stim.item);
+	  console.log(this.stim);
+    var article = getArticleItem(stim.item);
+   //  console.log(stim.item);
+   //  console.log(stim.label);
+	var contextsentence = "Vật đó có màu gì?";
+	//var contextsentence = "How typical is this for "+stim.basiclevel+"?";
+	//var objimagehtml = '<img src="images/'+stim.basiclevel+'/'+stim.item+'.jpg" style="height:190px;">';
+	var objimagehtml = '<img src="images/'+stim.label+'.jpg" style="height:230px;">';
+
+	$("#contextsentence").html(contextsentence);
+	$("#objectimage").html(objimagehtml);
+
+//ENTER TO CONTINUE/
+    document.onkeypress = checkKey;
+    function checkKey(e) {
+      e = e || window.event;
+      if (e.keyCode == 13) {
+         _s.button();
+      }
+    }
+	},
+
+	button : function() {
+	  if ($("#answer").val().length > 2) {
+        $(".err").hide();
+        this.log_responses();
+        _stream.apply(this); //use exp.go() if and only if there is no "present" data.
+      //}
+      } else {
+        $(".err").show();
+        document.getElementById('answer').value = '';
+      }
+    },
+
+    log_responses : function() {
+        exp.data_trials.push({
+          "slide_number_in_experiment" : exp.phase,
+          "utterance": this.stim.item,
+          "object": this.stim.label,
+          "rt" : Date.now() - _s.trial_start,
+          "response" : $("#answer").val()
+        });
+    }
+  });
+
+  slides.subj_info =  slide({
+    name : "subj_info",
+    start : function(e){
+      $(".err2").hide();
+    },
+    submit : function(e){
+      //if (e.preventDefault) e.preventDefault(); // I don't know what this means.
+      exp.subj_data = {
+        gender : $("#gender").val(),
+        age : $("#age").val(),
+        education : $("#education").val(),
+        asses : $('input[name="assess"]:checked').val(),
+        problems: $("#problems").val(),
+        fairprice: $("#fairprice").val(),
+        enjoyment : $("#enjoyment").val(),
+        comments : $("#comments").val(),
+
+        firstLanguage : $("#firstLanguage").val(),
+        VietPrimaryLanguageSchool : $("#VietPrimaryLanguageSchool").val(),
+        otherLanguage : $("#otherLanguage").val(),
+        liveInVietnam : $("#liveInVietnam").val(),
+        country : $("#country").val(),
+        dialectOne : $("#dialectOne").val(),
+        dialectTwo : $("#dialectTwo").val(),
+      };
+
+      // The second part of the questionaire is not optional throw an
+      // error if any of the questions in the second part are left unanswered
+      if (exp.subj_data.firstLanguage != "" &
+        exp.subj_data.otherLanguage != "" &
+        exp.subj_data.VietPrimaryLanguageSchool != "" &
+        exp.subj_data.liveInVietnam != "" &
+        exp.subj_data.country != "" &
+        exp.subj_data.dialectOne != "" &
+        exp.subj_data.dialectTwo != "") {
+        $(".err2").hide();
+        exp.go(); //use exp.go() if and only if there is no "present" data.
+      } else {
+        $(".err2").show();
+      };
+    }
+  });
+
+  slides.thanks = slide({
+    name : "thanks",
+    start : function() {
+      exp.data= {
+          "trials" : exp.data_trials,
+          "catch_trials" : exp.catch_trials,
+          "system" : exp.system,
+          "condition" : exp.condition,
+          "subject_information" : exp.subj_data,
+          "time_in_minutes" : (Date.now() - exp.startT)/60000
+      };
+      proliferate.submit(exp.data);
+    }
+  });
+
+  return slides;
+}
+
+/*
+INIT
+This function creates the stimuli list
+
+allstimuli is an array from 'stimuli.js' that contains all the stimuli in the
+study. Each entry in the array is in the following format:
+{
+"item": ["armchair"],
+"colorScheme": 1
+},
+item: an array with a single string containing the name of the object.
+colorScheme: {1,2} refers to the possible colors that that stimuli occurs in
+    1: blue, red, yellow, white
+    2: purple, orange, green, black
+Stimuli image names are of the format color_object.jpg
+
+The function takes in allStimuli variable and shuffles the order to randomize
+the order in which participants see stimuli.
+
+It then randomly assigns a color to each object (from the object's color scheme).
+
+Return:
+Dictionary with entries:
+  item: name of object, single word string
+  label: string of format "color_object"
+
+the ".jpeg" is added to the label in another part of the code
+*/
+function init() {
+  //get allStimuli
+  var items_target = _.shuffle(allStimuli);
+  //function that makes a dictionary of the desired output
+  // format for a single stimulis
+  function makeTargetStim(i) {
+    //get item
+    var item = items_target[i];
+    var item_id = item.item[0];
+    var cs1 = ["blue", "red", "yellow", "white"];
+    var cs2 = ["purple", "orange", "green", "black"];
+    var color = "";
+    object_color_scheme = item.colorScheme;
+
+    if (object_color_scheme == 1) {
+      color = cs1[Math.floor(Math.random() * Math.floor(4))];
+    } else {
+      color = cs2[Math.floor(Math.random() * Math.floor(4))];
+    }
+    var object_label = item.label;
+
+      return {
+	  "item": item_id,
+    "label": color + "_" + item_id
+    }
+  }
+
+  // Create empty array to store all the stims
+  exp.all_stims = [];
+  //for loop that iterates through all stimuli and calls on the function that
+  // creates a dictionary for each stimulus
+  for (var i=0; i < items_target.length; i++) {
+    //call on a function that creates the stims
+    // and add them to the empty array
+    exp.all_stims.push(makeTargetStim(i));
+  }
+
+  // shuffle the order of items in the array to get a randomized trial order
+  exp.all_stims = _.shuffle(exp.all_stims);
+
+  exp.trials = [];
+  exp.catch_trials = [];
+  exp.condition = {}; //can randomize between subject conditions here
+  exp.system = {
+      Browser : BrowserDetect.browser,
+      OS : BrowserDetect.OS,
+      screenH: screen.height,
+      screenUH: exp.height,
+      screenW: screen.width,
+      screenUW: exp.width
+    };
+
+  //blocks of the experiment:
+  exp.structure=["i0", "objecttrial", 'subj_info', 'thanks'];
+
+  exp.data_trials = [];
+  //make corresponding slides:
+  exp.slides = make_slides(exp);
+
+  exp.nQs = utils.get_exp_length(); //this does not work if there are stacks of stims (but does work for an experiment with this structure)
+                    //relies on structure and slides being defined
+  $(".nQs").html(exp.nQs);
+
+  $('.slide').hide(); //hide everything
+
+  //make sure turkers have accepted HIT (or you're not in mturk)
+  $("#start_button").click(function() {
+      exp.go();
+  });
+
+  exp.go(); //show first slide
+}
---
+++ simulations/ang_manh_simulations/models/scil_simulations_manh.R
@@ -0,0 +1,292 @@
+library(tidyverse)
+library(grid)
+library(gridExtra)
+library(cowplot)
+library(viridis)
+library(jsonlite)
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+
+# RUN WEBPPL FROM A V8 JS ENGINE (FASTER WHEN YOU NEED TO RUN MANY, MANY CALLS TO WEBPPL)
+
+source("../../../_shared/V8wppl.R")
+
+# SOURCE SOME HELPER SCRIPTS
+
+source("../../../_shared/simulationHelpers.R")
+
+# SOURCE THE ENGINE
+
+engine <- read_file("../../../_shared/engine.txt")
+
+modelAndSemantics <- read_file("manh_semantics.txt")
+
+# STATES
+
+states_ss = c("smallbluepin", "bigbluepin", "bigredpin", "smallblueball", "bigredpin", "bigredpin", "bigredpin", "bigredpin")
+states_cs = c("smallbluepin", "smallredpin", "bigredpin", "smallblueball", "bigredpin", "bigredpin", "bigredpin", "bigredpin")
+
+states_color <- "smallbluepin, smallredpin, bigredpin, smallblueball, bigredpin, bigredpin, bigredpin, bigredpin"
+states_size <- "smallbluepin, bigbluepin, bigredpin, smallblueball, bigredpin, bigredpin, bigredpin, bigredpin"
+
+# UTTERANCES: SIZE SUFFICIENT
+
+utterances_eng_ss <- c("START red pin STOP", "START blue pin STOP", 
+                       "START big pin STOP", "START small pin STOP",
+                       "START small ball STOP", "START blue ball STOP",
+                       "START pin STOP", "START ball STOP",
+                       "START big blue pin STOP", 
+                       "START big red pin STOP",
+                       "START small blue pin STOP",
+                       "START small blue ball STOP") 
+
+utterances_sp_ss <- c("START pin STOP", "START ball STOP",
+                            "START pin red STOP", "START pin blue STOP", "START ball blue STOP", 
+                            "START pin big STOP", "START pin small STOP","START ball small STOP",
+                            "START pin blue small STOP", 
+                            "START pin blue big STOP",
+                            "START pin red big STOP", "START ball blue small STOP") 
+
+utterances_fr_ss <- c("START pin STOP", "START ball STOP",
+                      "START pin red STOP", "START pin blue STOP","START ball blue STOP", 
+                           "START big pin STOP", "START small pin STOP", "START small ball STOP",
+                           "START big pin blue STOP",
+                           "START small pin blue STOP",
+                           "START big pin red STOP",
+                           "START small ball blue STOP")
+
+utterances_vt_ss <- c("START pin STOP", "START ball STOP", "START pin red STOP", "START pin blue STOP", "START ball blue STOP", 
+                              "START pin big STOP", "START pin small STOP","START ball small STOP",
+                              "START pin blue and big STOP",
+                              "START pin big and blue STOP",
+                              "START pin red and big STOP", "START pin big and red STOP",
+                              "START pin blue and small STOP", "START pin small and blue STOP",
+                      "START ball blue and small STOP", "START ball small and blue STOP")
+
+# UTTERANCES: COLOR SUFFICIENT
+
+utterances_eng_cs <- c("START pin STOP", "START ball STOP","START red pin STOP", "START blue pin STOP","START blue ball STOP", 
+                       "START big pin STOP", "START small pin STOP","START small ball STOP",
+                       "START big red pin STOP",
+                       "START small blue pin STOP","START small blue ball STOP",
+                       "START small red pin STOP")
+
+utterances_sp_cs <- c("START pin STOP", "START ball STOP","START pin red STOP", "START pin blue STOP", "START ball blue STOP", 
+                            "START pin big STOP", "START pin small STOP","START ball small STOP",
+                            "START pin red small STOP", "START pin red big STOP",
+                            "START pin blue small STOP", 
+                            "START ball blue small STOP")
+
+utterances_fr_cs <- c("START pin STOP", "START ball STOP","START pin red STOP", "START pin blue STOP", "START ball blue STOP", 
+                           "START big pin STOP", "START small pin STOP", "START small ball STOP",
+                           "START big pin red STOP",
+                           "START small pin red STOP",
+                           "START small pin blue STOP", 
+                           "START small ball blue STOP")
+
+utterances_vt_cs <- c("START pin STOP", "START ball STOP","START pin red STOP", "START pin blue STOP","START ball blue STOP", 
+                              "START pin big STOP", "START pin small STOP","START ball small STOP",
+                              "START pin red and big STOP","START pin big and red STOP",
+                      "START pin blue and small STOP", "START pin small and blue STOP", 
+                      "START ball blue and small STOP", "START ball small and blue STOP", 
+                              "START pin red and small STOP", "START pin small and red STOP")
+
+
+# COMMANDS
+
+cmd_eng = 'incrementalUtteranceSpeaker("START small blue pin STOP", "smallbluepin", model, params, semantics)'
+cmd_sp = 'incrementalUtteranceSpeaker("START pin blue small STOP", "smallbluepin", model, params, semantics)'
+cmd_fr = 'incrementalUtteranceSpeaker("START small pin blue STOP", "smallbluepin", model, params, semantics)'
+cmd_vt = 'incrementalUtteranceSpeaker("START pin blue and small STOP", "smallbluepin", model, params, semantics) + incrementalUtteranceSpeaker("START pin small and blue STOP", "smallbluepin", model, params, semantics)'
+
+# VALDF FOR SCIL PAPER
+
+valDF <- data.frame("colorNoise" = c(0.95), "sizeNoise" = c(0.8), "alpha" = c(1,5,10, 20))
+valDF <- valDF %>%
+  expand(colorNoise, sizeNoise, alpha) %>%
+  filter(alpha %in% c(1,5,10, 20))
+
+# VALDF FOR SCIL APP
+
+valDF <- data.frame("colorNoise" = c(0.95), "sizeNoise" = c(0.8), "alpha" = c(1,5,10, 20))
+valDF <- valDF %>%
+  expand(colorNoise, sizeNoise, alpha)
+
+# COLOR-SUFFICIENT SCENARIO 
+
+## English
+
+english_sizeOvermodification <- valDF %>%
+  group_by(colorNoise, sizeNoise, alpha) %>%
+  mutate(speakerProb = runModel('V8', engine, modelAndSemantics, cmd_eng, states_cs, utterances_eng_cs, alpha, sizeNoiseVal = sizeNoise, colorNoiseVal = colorNoise, 
+                                colorCost = 0, sizeCost = 0, nounCost = 0))
+
+english_sizeOvermodification = mutate(english_sizeOvermodification, state = states_color, .before = colorNoise)
+english_sizeOvermodification = mutate(english_sizeOvermodification, nounNoise = 0.99, .before = alpha)
+ english_sizeOvermodification$language <- "English"
+
+## Spanish
+
+sp_sizeOvermodification <- valDF %>%
+  group_by(colorNoise, sizeNoise, alpha) %>%
+  mutate(speakerProb = runModel('V8', engine, modelAndSemantics, cmd_sp, states_cs, utterances_sp_cs, alpha, sizeNoiseVal = sizeNoise, colorNoiseVal = colorNoise, 
+                                colorCost = 0, sizeCost = 0, nounCost = 0))
+
+sp_sizeOvermodification = mutate(sp_sizeOvermodification, state = states_color, .before = colorNoise)
+sp_sizeOvermodification = mutate(sp_sizeOvermodification, nounNoise = 0.99, .before = alpha)
+sp_sizeOvermodification$language <- "Spanish"
+
+## French
+
+fr_sizeOvermodification <- valDF %>%
+  group_by(colorNoise, sizeNoise, alpha) %>%
+  mutate(speakerProb = runModel('V8', engine, modelAndSemantics, cmd_fr, states_cs, utterances_fr_cs, alpha, sizeNoiseVal = sizeNoise, colorNoiseVal = colorNoise, 
+                                colorCost = 0, sizeCost = 0, nounCost = 0))
+
+fr_sizeOvermodification = mutate(fr_sizeOvermodification, state = states_color, .before = colorNoise)
+fr_sizeOvermodification = mutate(fr_sizeOvermodification, nounNoise = 0.99, .before = alpha)
+fr_sizeOvermodification$language <- "French"
+
+## Vietnamese
+
+vt_sizeOvermodification <- valDF %>%
+  group_by(colorNoise, sizeNoise, alpha) %>%
+  mutate(speakerProb = runModel('V8', engine, modelAndSemantics, cmd_vt, states_cs, utterances_vt_cs, alpha, sizeNoiseVal = sizeNoise, colorNoiseVal = colorNoise, 
+                                colorCost = 0, sizeCost = 0, nounCost = 0))
+
+vt_sizeOvermodification = mutate(vt_sizeOvermodification, state = states_color, .before = colorNoise)
+vt_sizeOvermodification = mutate(vt_sizeOvermodification, nounNoise = 0.99, .before = alpha)
+vt_sizeOvermodification$language <- "Vietnamese"
+
+sizeOvermodification <- rbind(english_sizeOvermodification, rbind(sp_sizeOvermodification,rbind(fr_sizeOvermodification,vt_sizeOvermodification)))
+
+# SIZE-SUFFICIENT SCENARIO
+
+## English
+
+english_colorOvermodification <- valDF %>%
+  group_by(colorNoise, sizeNoise, alpha) %>%
+  mutate(speakerProb = runModel('V8', engine, modelAndSemantics, cmd_eng, states_ss, utterances_eng_ss, alpha, sizeNoiseVal = sizeNoise, colorNoiseVal = colorNoise,
+                                colorCost = 0, sizeCost = 0, nounCost = 0))
+
+english_colorOvermodification = mutate(english_colorOvermodification, state = states_size, .before = colorNoise)
+english_colorOvermodification = mutate(english_colorOvermodification, nounNoise = 0.99, .before = alpha)
+english_colorOvermodification$language <- "English"
+
+## Spanish
+
+sp_colorOvermodification <- valDF %>%
+  group_by(colorNoise, sizeNoise, alpha) %>%
+  mutate(speakerProb = runModel('V8', engine, modelAndSemantics, cmd_sp, states_ss, utterances_sp_ss, alpha, sizeNoiseVal = sizeNoise, colorNoiseVal = colorNoise,
+                                colorCost = 0, sizeCost = 0, nounCost = 0))
+
+sp_colorOvermodification = mutate(sp_colorOvermodification, state = states_size, .before = colorNoise)
+sp_colorOvermodification = mutate(sp_colorOvermodification, nounNoise = 0.99, .before = alpha)
+sp_colorOvermodification$language <- "Spanish"
+
+## French
+
+fr_colorOvermodification <- valDF %>%
+  group_by(colorNoise, sizeNoise, alpha) %>%
+  mutate(speakerProb = runModel('V8', engine, modelAndSemantics, cmd_fr, states_ss, utterances_fr_ss, alpha, sizeNoiseVal = sizeNoise, colorNoiseVal = colorNoise,
+                                colorCost = 0, sizeCost = 0, nounCost = 0))
+fr_colorOvermodification = mutate(fr_colorOvermodification, state = states_size, .before = colorNoise)
+fr_colorOvermodification = mutate(fr_colorOvermodification, nounNoise = 0.99, .before = alpha)
+fr_colorOvermodification$language <- "French"
+
+## Vietnamese
+
+vt_colorOvermodification <- valDF %>%
+  group_by(colorNoise, sizeNoise, alpha) %>%
+  mutate(speakerProb = runModel('V8', engine, modelAndSemantics, cmd_vt, states_ss, utterances_vt_ss, alpha, sizeNoiseVal = sizeNoise, colorNoiseVal = colorNoise,
+                                colorCost = 0, sizeCost = 0, nounCost = 0))
+
+vt_colorOvermodification = mutate(vt_colorOvermodification, state = states_size, .before = colorNoise)
+vt_colorOvermodification = mutate(vt_colorOvermodification, nounNoise = 0.99, .before = alpha)
+vt_colorOvermodification$language <- "Vietnamese"
+
+colorOvermodification <- rbind(english_colorOvermodification, rbind(sp_colorOvermodification,rbind(fr_colorOvermodification,vt_colorOvermodification)))
+
+write.csv(colorOvermodification, "../series/series1/model_output/color_overmodification_six_red_pin_no_cost_medium_var.csv", row.names = FALSE)
+write.csv(sizeOvermodification, "../series/series1/model_output/size_overmodification_six_red_pin_no_cost_medium_var.csv", row.names = FALSE)
+#view(modelAndSemantics)
+
+# # PREDICTIONS PLOTS
+# 
+# plot <- function(probDF) {
+#   probDF$speakerProb <- as.numeric(probDF$speakerProb)
+#   p <- ggplot(probDF, aes(x=sizeNoise,y=colorNoise,color=speakerProb)) +
+#     geom_point(size=5,shape=15) +
+#     scale_x_continuous(limits=c(.45,1.0),breaks=seq(.475,1.0,.525),labels=c(0.5,1)) +
+#     scale_y_continuous(limits=c(.45,1.0),breaks=seq(.475,1.0,.525),labels=c(0.5,1)) +
+#     scale_colour_viridis(limits=c(0,1), name="Probability of\nutterance") +
+#     facet_grid(alpha~language) +
+#     xlab("Semantic value of size") +
+#     ylab("Semantic value of color") +
+#     theme(panel.spacing=unit(.25, "lines"),
+#           panel.border = element_rect(color = "black", fill = NA, size = 1),
+#           # axis.text.x = element_text(angle = 20, hjust=1),
+#           axis.text.y = element_text(hjust=0.5)) +
+#     xlab(element_blank()) +
+#     ylab(element_blank())
+#   return(p)
+# }
+# 
+# color_plot <- plot(colorOvermodification) +
+#   theme(strip.text.y = element_blank(),
+#         legend.position = "none") +
+#   ggtitle("Redundant color modification")
+# 
+# size_plot <- plot(sizeOvermodification) +
+#   theme(axis.text.y = element_blank(),
+#         axis.ticks.y = element_blank(),
+#         legend.position = "none") +
+#   ylab(element_blank()) +
+#   ggtitle("Redundant size modification")
+# 
+# legend <- plot_grid(get_legend(color_plot + theme(legend.position = "right")))
+# 
+# graphs <- arrangeGrob(grobs = list(color_plot, size_plot), ncol = 2, bottom = 'Semantic value of size', left = 'Semantic value of color', right = 'Alpha')
+# 
+# g <- arrangeGrob(graphs, legend, ncol = 2, widths = c(0.85, 0.15))
+# 
+# ggsave(g, filename = "scilpreds.pdf", height = 4, width = 8, units = "in", dpi = 1000)
+# 
+# ### SCIL MODEL COMPARISON
+# 
+# ### W/ RENAMING FOR NSF APPLICATION
+# 
+# base = 6
+# expand = 3
+# 
+# graph <- function(probArray) {
+# 
+#   toGraph <- data.frame(matrix(NA, nrow = 4, ncol = 3))
+#   colnames(toGraph) <- c("language", "behavior", "probability")
+#   # toGraph$language <- c("English", "English", "Spanish-postnom.", "Spanish-postnom.")
+#   toGraph$language <- c("English", "English", "Spanish", "Spanish")
+#   # toGraph$behavior <- c("Redundant color adjective (SS)", "Redundant size adjective (CS)",
+#                         # "Redundant color adjective (SS)", "Redundant size adjective (CS)")
+#   # LABELS FOR POSTER
+#   toGraph$behavior <- c("Redundant color adjective", "Redundant size adjective",
+#                         "Redundant color adjective", "Redundant size adjective")
+#   toGraph$probability <- probArray
+# 
+#   p <- ggplot(toGraph, aes(x=language, y=probability, fill = behavior)) +
+#     theme_bw() +
+#     theme(text = element_text(size = base * expand / 2, face = "bold")) +
+#     ylab(element_blank()) +
+#     xlab(element_blank()) +
+#     geom_bar(stat="identity",position = "dodge") +
+#     # scale_fill_viridis(discrete = TRUE) +
+#     # color for the poster
+#     scale_fill_manual(values=c("#4287f5","#fff200")) +
+#     # for hypothetical graphs
+#     theme(legend.title = element_blank(), legend.position="none", # axis.text.x = element_blank(),
+#           axis.text.x = element_text(angle = 20, hjust=1),
+#     )
+# 
+#   return(p)
+
+
