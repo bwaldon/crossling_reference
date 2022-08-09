8a2e3df3bc59c2b79d408542a590c30e4b5cf3b9
Angelique <acharles-davis00@stuy.edu>
2022-08-09 15:45:43 -0700

    added to scene generator-- builds image paths

--- experiments/from-scratch/server/FRENCH/buildScene.js
+++ experiments/from-scratch/server/FRENCH/buildScene.js
@@ -706,23 +706,25 @@ const scenes = [
   }
 ]// all of these will be imported in the real thing, this is just for coding purposes
 //TODO: import real lists
-femPool = ["fork", "key", "remote", "brush"]
-mascPool = ["bike", "comb", "armchair", "duck"]
+femAll = ["fork", "key", "remote","belt","candle","calculator","cap","chair","clock","crown", "tie"
+, "tent", "spoon", "sock", "shovel", "scarf", "ruler", "ring", "ornament", "napkin", "mug", "mouse", "lamp", "fryingpan",
+"flower","guitar", "dress", "door", //rest are exclusive to french
+"billiardball", "magnifyingglass", "stapler", "ladle"]
+mascAll = ["airplane", "bike", "comb", "armchair",
+"balloon", "bed","bonbon","bucket","butterfly","cake", "calendar","coathanger","cushion"
+,"die", "drum","fish","frame","hammer","knife","lipstick","lock","mask","microscope","pencil","phone"
+,"present","piano","whistle", "truck", "vase", "switch", "shell", "robot", "razor", "rug"]
+
+
 const colorPool = ["red", "yellow", "orange", "green", "blue", "purple", "black", "white"]
 
-
-function pickNoun(gender) {
-  noun = ""
-  if (gender == "F"){
-    return femPool[Math.floor(Math.random() * femPool.length)];
-    //TODO remove values from the pools after
-  } else {
-    return mascPool[Math.floor(Math.random() * mascPool.length)];
-    //TODO remove values from the pools after
-  }
+//picks a random noun from an array
+function pickNoun(pool) {
+    noun = pool[Math.floor(Math.random() * pool.length)];
+    //TODO remove noun from list
   return noun
 }
-
+//selects a random color and another different color
 function pickColor() {
   return colorPool[Math.floor(Math.random() * colorPool.length)];
 }
@@ -731,21 +733,57 @@ function pickColorExcept(color) {
   return newPool[Math.floor(Math.random() * newPool.length)];
 }
 
+//assign colors, sizes and types to the objects in the template
 function generateScenes(sceneTemplate) {
-  newScenes = sceneTemplate
+  newScenes = []
+  femPool = femAll
+  mascPool = mascAll
   for (index in sceneTemplate) {
-    sceneName = newScenes[index]["Name"]
+    sceneName = sceneTemplate[index]["Name"]
     // pick values
     color_1 = pickColor()
     color_2 = pickColorExcept(color_1)
-    type_1 = sceneName.includes("_m")? pickNoun("M") : pickNoun("F")
-    type_2 = sceneName.includes("_f")? pickNoun("F") : pickNoun("M")
+    type_1 = sceneName.includes("_m")? pickNoun(mascPool) : pickNoun(femPool)
+    mascPool = mascPool.filter(x => x != type_1)//no replacement
+    femPool = femPool.filter(x => x != type_1)
+    type_2 = sceneName.includes("_m")? pickNoun(mascPool) : pickNoun(femPool)
+    mascPool = mascPool.filter(x => x != type_2)//no replacement
+    femPool = femPool.filter(x =>  x != type_2)
     size_1 = index%2 == 0? "small" : "big"
-    size_1 = index%2 == 1? "small" : "big"
+    size_2 = index%2 == 1? "small" : "big"
     // assign the values
-    newScenes = newScenes.replace("color_1", color_1)
+    sceneString = JSON.stringify(sceneTemplate[index])
+    sceneString = sceneString.replaceAll("color_1", color_1)
+    sceneString = sceneString.replaceAll("color_2", color_2)
+    sceneString = sceneString.replaceAll("size_1", size_1)
+    sceneString = sceneString.replaceAll("size_2", size_2)
+    sceneString = sceneString.replaceAll("type_1", type_1)
+    sceneString = sceneString.replaceAll("type_2", type_2)
+    //revert to Json and push new scene
+    sceneToAdd = JSON.parse(sceneString)
+    newScenes.push(sceneToAdd)
+  }
+  //newScenes = _.shuffle(newScenes) --> does not work without importing the _ library in other files
+  return(newScenes)
+}
+
+// creating scenes as a list of object paths to image files
+function makePaths(objectList){
+  newScenes = []
+  for (index in objectList){
+    scene = objectList[index]
+    Objnew = {"Name" : scene["Name"]}
+    for (let i = 1 ; i<7 ; i ++){
+      name = "Obj" + i
+      path = scene[name+"_size"]+"_"+scene[name+"_color"]+"_"+scene[name+"_type"]+".jpg"
+      if (name == scene["Target"]) name = "Target"
+      Objnew[name] = path != "__.jpg"? path : ""
+    }
+    newScenes.push(Objnew)
   }
   return(newScenes)
 }
 
-console.log(generateScenes(scenes))
+newScenes = generateScenes(scenes)
+scenePaths = makePaths(newScenes)
+console.log(scenePaths)
