48d823c9262df4fd04900b8a761734f637bdd93e
Manh <manh08@stanford.edu>
2022-08-12 12:44:40 -0700

    changing item list so it will not produce undefined images

--- experiments/from-scratch/server/FRENCH/nounToScheme.js
+++ experiments/from-scratch/server/FRENCH/nounToScheme.js
@@ -5,11 +5,11 @@ export const nounToScheme = {
 "die": 0, "drum" : 0,"fish": 0,"frame":1 ,"hammer": 0,"knife": 0,
 "lipstick": 0,"lock": 0,"mask":1,"microscope": 0,"pencil": 1,"phone": 1
 ,"present": 1,"piano": 0,"whistle":0, "truck":0, "vase": 0, "switch":1,
-"shell": 0, "robot": 0, "razor": 0, "rug":1,
+"shell": 0, "robot": 0, "razor": 0, "rug":1, "screwdriver": 0,
 "fork": 0, "key":1, "remote":0,"belt":1,"candle":1,"calculator":1,"cap":1,
 "chair":1,"clock":1,"crown":1, "tie":0,
 "tent": 0, "spoon":0, "sock":0, "shovel":0, "scarf":1, "ruler":1, "ring":1,
-"ornament":1, "napkin":1, "mug":1, "mouse":1, "lamp":1, "fryingpan":1,
+"ornament":1, "napkin":1, "mug":1, "mouse":1, "lamp":1, "pan":1,
 "flower":1,"guitar":1, "dress":1, "door":1, //rest are exclusive to french
 "billiardball":1, "magnifyingglass":1, "stapler":1, "ladle":0
 }
