15ec7341ad48b766f9bcc3de458152f557da2cf2
Angelique <acharles-davis00@stuy.edu>
2022-08-19 12:04:09 -0700

    added English regression script

--- analyses/ENGLISH2022_summer/NounInformative/pilot/rscripts/regression.R
+++ analyses/ENGLISH2022_summer/NounInformative/pilot/rscripts/regression.R
@@ -11,11 +11,11 @@ setwd(this.dir)
 # color-blind-friendly palette
 cbPalette <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
-source("../../../_shared/regressionHelpers.r")
+source("../../../../_shared/regressionHelpers.r")
 
 # READ DATA
 # # (dummy data for analysis pipeline creation)
-d = read_delim("../../../../data/FRENCH/data_exp1.tsv", delim = "\t")
+d = read_delim("../../../../../data/ENGLISH2022_summer/pilot/data_exp1.tsv", delim = "\t")
 #d_postraw = read_tsv("../../data/SpanishMain/postManualTypoCorrection.tsv") %>% bind_rows(read_tsv("../../data/SpanishMain/postManualTypoCorrection_part2.tsv"))
 
 # get data with linear order annotation
@@ -58,88 +58,94 @@ d = read_delim("../../../../data/FRENCH/data_exp1.tsv", delim = "\t")
 
 # PLOT PROPORTION OF REDUNDANT UTTERANCES BY REDUNDANT PROPERTY
 agr <- d %>%
-  select(redundant,Redundant_Property,NumDistractors,Distractors_Noun,Distractors_RedProp) %>%
   mutate(Condition = case_when(
-    NumDistractors == 3 ~ "base",
-    TRUE ~ paste(Distractors_Noun,"_noun/",Distractors_RedProp,"_redundantvalue",sep=""))) %>% 
+    TrialType == "target" & DistractorsNoun == "no_extras" ~ "base",
+    TrialType == "control" ~ ControlType,
+    TRUE ~ paste(DistractorsNoun,"_noun/",DistractorsRedProp,"_redundantvalue",sep=""))) %>% 
+  select(redundant,RedundantProperty,Condition,TrialType) %>%
+  # mutate(Condition = case_when(
+  # NumDistractors == 3 ~ "base",
+  # TRUE ~ paste(DistractorsNoun,"_noun/",DistractorsRedProp,"_redundantvalue",sep=""))) %>% 
   #mutate(Redundant = case_when(redUtterance == "redundant" ~ 1,
   #TRUE ~ 0)) %>% 
   # pivot_longer(redUtterance,names_to="Utterance",values_to="Mentioned",-Redundant_Property,-NumDistractors,-Distractors_Noun,-Distractors_RedProp) %>%
-  group_by(Redundant_Property,Condition) %>%
+  group_by(RedundantProperty,TrialType,Condition) %>%
   summarise(Probability=mean(redundant),ci.low=ci.low(redundant),ci.high=ci.high(redundant)) %>%
   ungroup() %>%
   mutate(YMin = Probability - ci.low, YMax = Probability + ci.high) %>% 
-  mutate(Condition = fct_relevel(Condition, "base"),Redundant_Property=fct_recode(Redundant_Property,color="color redundant",size="size redundant"))
+  mutate(Condition = fct_relevel(Condition, "noun_sufficient","size_redundant","color_redundant"),
+         RedundantProperty=fct_recode(RedundantProperty,color="color redundant",size="size redundant"))
 
-ggplot(agr, aes(x=Condition,y=Probability,color=Redundant_Property,group=1)) +
+ggplot(agr, aes(x=Condition,y=Probability,color=RedundantProperty,group=1)) +
   geom_point() +
   geom_errorbar(aes(ymin=YMin,ymax=YMax)) +
   xlab("Condition") +
-  ylab("Probability of redundant modifier") +
+  ylab("Proportion of color-and-size mentions") +
   scale_color_manual(name="Redundant\nproperty",values=cbPalette[1:2]) +
+  facet_wrap(~TrialType,scales="free") +
   theme(axis.text.x=element_text(angle=45,hjust=1,vjust=1))
 
-ggsave(file="../graphs/redundant_proportions.pdf",width=6,height=7)
+ggsave(file="../graphs/redundant_proportions.pdf",width=8,height=7)
 
 
 
 
 
 # BAYESIAN MIXED EFFECTS LOGISTIC REGRESSION
-## READ IN THE ENGLISH DATA FROM DEGEN ET AL. (2020)
 
-d_french <- read_delim("../../../../data/FRENCH/data_exp1.tsv", delim = "\t")
-d_french$Language <- "French"
-d_french = d_french %>% 
+d_english <- read_delim("../../../../../data/ENGLISH2022_summer/pilot/data_exp1.tsv", delim = "\t")
+d_english$Language <- "English"
+d_english = d_english %>% 
   mutate(NounMentioned = case_when(typeMentioned == TRUE ~ "noun",
                                    TRUE ~ "no noun"))
 
-d = d_french # might want to eventually add additional languages here
+d = d_english # might want to eventually add additional languages here
 
 # # CENTER PREDICTORS (NOTE: REFERENCE LEVEL OF FACTORS MAY CHANGE)
 d <- d %>% 
-  mutate(Redundant_Property = factor(Redundant_Property),
+  mutate(RedundantProperty = factor(RedundantProperty),
          redUtterance = factor(redUtterance),
-         Distractors_Noun = factor(Distractors_Noun),
-         Distractors_RedProp = factor(Distractors_RedProp),
+         DistractorsNoun = factor(DistractorsNoun),
+         DistractorsRedProp = factor(DistractorsRedProp),
          gameid = factor(gameid),
          Language = factor(Language),
-         Item = factor(clickedType)) # currently an item is just defined in terms of target object type -- extend to target and competitor combo
+         Item = factor(ItemID)) # edited:
 
 # different subset analyses:
 d_extended = d %>% 
-  filter(NumDistractors == 5) %>%  # ultimately also filter by trial type & TrialType == "target")
+  filter(NumDistractors == 5) %>%  
+  filter(TrialType == "target") %>% #edited:
   droplevels()
 
 # first test effects of redundant property and number of distractors on full dataset
-centered_number = cbind(d,myCenter(data.frame(d %>% select(Redundant_Property, Trial, NumDistractors )))) #Language))))
+centered_number = cbind(d,myCenter(data.frame(d %>% select(RedundantProperty, Trial, NumDistractors )))) #Language))))
 
 contrasts(centered_number$redUtterance)
-contrasts(centered_number$Redundant_Property)
+contrasts(centered_number$RedundantProperty)
 #contrasts(centered_number$Language)
 
 # the model with the full random effects structure (which can't run on this few data points)
-m_number = glmer(redundant ~ cRedundant_Property*cNumDistractors + cTrial + (1 + cRedundant_Property*cNumDistractors + cTrial|gameid) + (1 + cRedundant_Property*cNumDistractors + cTrial|Item), data=centered_number, family="binomial")
+m_number = glmer(redundant ~ cRedundantProperty*cNumDistractors + cTrial + (1 + cRedundantProperty*cNumDistractors + cTrial|gameid) + (1 + cRedundantProperty*cNumDistractors + cTrial|Item), data=centered_number, family="binomial")
 summary(m_number)
 
 # the model with only random intercepts for testing
-m_number = glmer(redundant ~ cRedundant_Property*cNumDistractors + cTrial + (1|gameid) + (1|Item), data=centered_number, family="binomial")
+m_number = glmer(redundant ~ cRedundantProperty*cNumDistractors + cTrial + (1|gameid) + (1|Item), data=centered_number, family="binomial")
 summary(m_number)
 
 
 # now test effects of distractor objects on subset with 6 objects in display
-centered_extended = cbind(d_extended,myCenter(data.frame(d_extended %>% select(Redundant_Property, Trial, Distractors_Noun, Distractors_RedProp)))) #Language))))
+centered_extended = cbind(d_extended,myCenter(data.frame(d_extended %>% select(RedundantProperty, Trial, DistractorsNoun, DistractorsRedProp)))) #Language))))
 
 contrasts(centered_extended$redUtterance)
-contrasts(centered_extended$Redundant_Property)
-contrasts(centered_extended$Distractors_Noun)
-contrasts(centered_extended$Distractors_RedProp)
+contrasts(centered_extended$RedundantProperty)
+contrasts(centered_extended$DistractorsNoun)
+contrasts(centered_extended$DistractorsRedProp)
 #contrasts(centered_number$Language)
 
 # the model with the full random effects structure (which can't run on this few data points)
-m_extended = glmer(redundant ~ cRedundant_Property*cDistractors_Noun*cDistractors_RedProp + cTrial + (1 + cRedundant_Property*cDistractors_Noun*cDistractors_RedProp + cTrial|gameid) + (1 + cRedundant_Property*cDistractors_Noun*cDistractors_RedProp + cTrial|Item), data=centered_extended, family="binomial")
+m_extended = glmer(redundant ~ cRedundantProperty*cDistractorsNoun*cDistractorsRedProp + cTrial + (1 + cRedundantProperty*cDistractorsNoun*cDistractorsRedProp + cTrial|gameid) + (1 + cRedundantProperty*cDistractorsNoun*cDistractorsRedProp + cTrial|Item), data=centered_extended, family="binomial")
 summary(m_extended)
 
 # the model with only random intercepts for testing
-m_extended = glmer(redundant ~ cRedundant_Property*cDistractors_Noun*cDistractors_RedProp + cTrial +  (1|gameid) + (1|Item), data=centered_extended, family="binomial")
+m_extended = glmer(redundant ~ cRedundantProperty*cDistractorsNoun*cDistractorsRedProp + cTrial +  (1|gameid) + (1|Item), data=centered_extended, family="binomial")
 summary(m_extended)
