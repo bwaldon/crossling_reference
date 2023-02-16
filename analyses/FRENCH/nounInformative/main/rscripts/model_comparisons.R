77577c499aee4428c3d592fc26836b3fe3293e37
Angelique Charles-Davis <angelcd@stanford.edu>
2023-02-15 18:30:09 -0800

    edits to comparison script, r coefficient

--- analyses/FRENCH/nounInformative/main/rscripts/model_comparisons.R
+++ analyses/FRENCH/nounInformative/main/rscripts/model_comparisons.R
@@ -127,17 +127,55 @@ toGraph_fr <- getEmpiricalProb(toGraph_fr,2)
 toGraph_eng <- getEmpiricalProb(toGraph_eng,0)
 
 typeOrder = c("Discrete global", "Discrete incremental", "Continuous global", "Continuous incremental")
-makePlot = function(plot){
+makePlot = function(plot,lang,xnoun){
   ggplot(plot, aes(x=Model_output, y=Probability)) +
     #set_theme(base=theme_bw())
     geom_point(stat="identity") +
     scale_fill_manual(values =cbPalette) +
     facet_wrap(~factor(modelType,typeOrder), nrow = 2) +
     ylim(0,1) +
-    labs(y= "Experimental probability", x= "Model probability", title= "Modeling Comparisons") +
+    labs(y= "Experimental probability", x= sprintf("Model probability (xnoun=%1.2f)",xnoun), title= "Empirical vs. Modeling Probabilities") +
     theme(axis.text.x = element_text(angle=15,hjust=1,vjust=1),legend.position="bottom")
-  #ggsave(filename = filename, path = sprintf("../../graphs/w23/%s",filepath), device = "jpg")
+  ggsave(filename = sprintf("model_comp_%s_%1.2f.jpg",lang,xnoun), path = "../../../../../analyses/FRENCH/nounInformative/main/graphs", device = "jpg")
 }
 
-makePlot(toGraph_fr)
-makePlot(toGraph_eng)
+makePlot(toGraph_fr,"fr",0.99)
+makePlot(toGraph_eng, "eng",0.99)
+
+#Coefficient analysis
+findCor = function(toGraph){
+  for (type in typeOrder){
+    toFind <- toGraph %>% filter(modelType == type)
+    print(sprintf("%s: %f",type,cor(toFind$Model_output,toFind$Probability)))
+  }
+}
+findCor(toGraph_eng)
+findCor(toGraph_fr)
+
+#//--- Noun Noise = 0.90
+modeling_fr <- modeling %>% filter(Language == 2 | global_inc == "global")
+modeling_eng <- modeling %>% filter(Language == 0)
+
+modeling_fr <- modeling_fr %>% filter(noun_noise != 0.99)
+modeling_eng <- modeling_eng %>% filter(noun_noise != 0.99)
+
+modeling_fr <- getModelType(modeling_fr)
+toGraph_fr <- subset(modeling_fr, select= c(Name,modelType,output))
+toGraph_fr <- toGraph_fr %>% 
+  rename("Model_output" = "output")
+
+modeling_eng <- getModelType(modeling_eng)
+toGraph_eng <- subset(modeling_eng, select= c(Name,modelType,output))
+toGraph_eng <- toGraph_eng %>% 
+  rename("Model_output" = "output")
+
+toGraph_fr <- getEmpiricalProb(toGraph_fr,2)
+toGraph_eng <- getEmpiricalProb(toGraph_eng,0)
+
+#plotting
+makePlot(toGraph_fr,"fr",0.90)
+makePlot(toGraph_eng, "eng",0.90)
+
+#coefficient analysis
+findCor(toGraph_eng)
+findCor(toGraph_fr)
